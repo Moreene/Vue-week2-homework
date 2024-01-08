bf21d22bc67842678fc6c2e7525036ff9cb1ef42
Moreene <moreenewu@gmail.com>
2024-01-08 19:08:44 +0800

    v.1

---
+++ js/product.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            products: [],
+            selectedProduct: {},
+            isShow: false
+        };
+    },
+    methods: {
+        checkLogin() {
+            axios.post("https://ec-course-api.hexschool.io/v2/api/user/check")
+                .then(res => {
+                    this.getProduct();
+                    this.isShow = true;
+                })
+                .catch(err => {
+                    alert('您沒有權限進入!')
+                    location.href = "index.html";
+                });
+        },
+        getProduct() {
+            axios.get("https://ec-course-api.hexschool.io/v2/api/moreene/admin/products")
+                .then(res => {
+                    this.products = res.data.products;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        checkDetail(id) {
+            this.selectedProduct = this.products.find(item => item.id === id);
+        }
+    },
+    mounted() {
+        const token = document.cookie.replace(
+            /(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/, "$1",);
+        axios.defaults.headers.common['Authorization'] = token;
+        this.checkLogin();
+    },
+});
+
+app.mount("#app");
